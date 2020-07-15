1503979703b5dd9770869570493b0a907a4e8ca6
mromero <mgr_89@hotmail.com>
2020-07-15 11:20:30 -0300

    button comprar

--- src/components/producto.js
+++ src/components/producto.js
@@ -3,10 +3,20 @@ import React from 'react';
 const Producto = (props) => {
     const {id,nombre,precio}= props.producto
 
+    //agregar producto al carrito
+
+    const seleccionarProducto=(id)=>{
+        console.log('comprando....', id)
+    };
+
     return (  
         <div>
             <h2> {nombre} </h2>
             <p> {precio}</p>
+            <button 
+            type='button'
+            onClick={()=>seleccionarProducto(id)}
+            >Comprar</button>
 
         </div>
     
